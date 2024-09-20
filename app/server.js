4de110d624f52a1c999f1d5b135fdedc14d8f2f6
tagost <85510673+tagost@users.noreply.github.com>
2024-09-20 18:32:44 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World! V1');
+  res.send('Hello World! V2');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
